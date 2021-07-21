377d3126489aa7947aab4eae9bd263b668df726e
chattray <chea0073@gmail.com>
2021-07-20 19:22:15 -0700

    Server build

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const weather = require('./data/weather.json');
+
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.get('/weather', (req, res) => {
+  class Forecast {
+    constructor (date, description) {
+      this.date = date;
+      this.description = description;
+    }
+  }
+  let searchQuery = 'Seattle';
+  let queryData = weather.find(value => value.city_name === searchQuery);
+
+  let days = [];
+  console.log(queryData);
+  queryData.data.map((value) => days.push(new Forecast(value.datetime, `Low of ${value.low_temp}, High of ${value.high_temp}, with ${value.weather.description}`)
+  ));
+  res.send(days);
+});
+
+// let latLon = [weather[0].lat, weather[0].lon, weather[0].city_name];
+// app.get('/weather', (req, res) => {
+//   res.send(latLon);
+// });
+
+app.listen(PORT, () => console.log(`Running Server on ${PORT}`));
