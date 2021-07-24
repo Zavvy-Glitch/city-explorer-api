ba18d31b0acb0008d6169be0845dc06b60d10c0e
chattray <chea0073@gmail.com>
2021-07-23 18:06:22 -0700

    added cache

--- routes/movies.js
+++ routes/movies.js
@@ -1,36 +1,55 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache.js');
 
 async function getMovies(req, res) {
+  console.log(req.query, 'movies');
   const searchQuery = req.query.searchQuery;
+  const key = 'movies-' + searchQuery;
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEKEY}&language=en-US&query=${searchQuery}&page=1`;
-  try {
-    const movie = await axios.get(url);
-    const topMovies = movie.data.results;
-    const sortedTopMovies = topMovies.sort((a, b) => b.popularity - a.popularity);
 
-    const newMovieArray = [];
-    for(let i=0; i<20; i++){
-      newMovieArray.push(new Movie(sortedTopMovies[i].title, sortedTopMovies[i].overview, sortedTopMovies[i].vote_average, sortedTopMovies[i].vote_count, sortedTopMovies[i].poster_path, sortedTopMovies[i].popularity, sortedTopMovies[i].release_date ));
+  try {
+    console.log(cache);
+    if(!cache[key]) {
+      cache[key] = {};
+      cache[key].timestamp = Date.now();
+      axios.get(url)
+        .then(data => {
+        // const movie = await axios.get(url);
+          console.log(data);
+          const topMovies = data.data.results;
+          const sortedTopMovies = topMovies.sort((a, b) => b.popularity - a.popularity);
+
+          const newMovieArray = [];
+
+          class Movie {
+            constructor (title, overView, averageVotes, totalVotes, imageUrl, popularity, releasedOn){
+              this.title = title;
+              this.overView = overView;
+              this.averageVotes = averageVotes;
+              this.totalVotes = totalVotes;
+              this.imageUrl = imageUrl;
+              this.popularity = popularity;
+              this.releasedOn = releasedOn;
+            }
+          }
+          for(let i=0; i<20; i++){
+            newMovieArray.push(new Movie(sortedTopMovies[i].title, sortedTopMovies[i].overview, sortedTopMovies[i].vote_average, sortedTopMovies[i].vote_count, sortedTopMovies[i].poster_path, sortedTopMovies[i].popularity, sortedTopMovies[i].release_date ));
+          }
+          cache[key].data = newMovieArray;
+          // getMovies(data.data);
+          res.send(newMovieArray);
+        });
+
     }
-    console.log(newMovieArray.length);
-    res.send(newMovieArray);
+    else{
+      // console.log(newMovieArray.length);
+      res.send(cache[key].data);}
   }
   catch(err){
     res.send(500).send(err);
   }
 }
 
-class Movie {
-  constructor (title, overView, averageVotes, totalVotes, imageUrl, popularity, releasedOn){
-    this.title = title;
-    this.overView = overView;
-    this.averageVotes = averageVotes;
-    this.totalVotes = totalVotes;
-    this.imageUrl = imageUrl;
-    this.popularity = popularity;
-    this.releasedOn = releasedOn;
-  }
-}
 module.exports = getMovies;
